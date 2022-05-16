6816d12c644abed35726673ef66e5793d6ce6db3
Reetush10 <reetu.sh@appinessworld.com>
2022-05-16 12:03:16 +0530

    changes

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ function register()
     else
     alert("Registration Successful!!")
 }
+function img1(){
+    const newImage = document.getElementById('selectA').value;
+    document.getElementById('poll').innerHTML = x;
+}
+
+/* const Pg =[
+    Name: Pragati Pg
+    Address: Malleshwaram bangalore
+    State:Karnataka
+    City: Bagalore
+    Phone No: 6543278654
+
+
+]*/
